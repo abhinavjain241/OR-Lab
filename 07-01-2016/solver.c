cca85fb5a0ddaaece3029f7bf3aa6e362d1298fc
Abhinav Jain <abhinavjain241@gmail.com>
2016-01-14 13:47:50 +0530

    Made changes. Still incomplete

--- 07-01-2016/solver.c
+++ 07-01-2016/solver.c
@@ -2,66 +2,106 @@
 	Abhinav Jain
 	13MA20004
 	Date: 07-01-2016
+	Max:/ Min = Z = C^T X
+    Subject to AX= b
+    X >= 0
 */
 #include <stdio.h>
 #include <malloc.h>
 #include <assert.h>
+#include <math.h>
 
-void swap(float *, int, int, int);
-int calculateRank(float *, int, int);
-int printMatrix(float *, int, int);
-int getMatrix(float *, int, int);
+void swap(double **, int, int, int);
+int calculateRank(double **, int, int);
+void printMatrix(double **, int, int);
+void getMatrix(double **, int, int);
+double** augmentMatrices(double **, double **, int, int);
+long long C(int , int);
 
 int main() {
 	/* Get Objective Function Z */
 	int n;	//Number of variables
 	int m;	//Number of equations
+	int i, j; // Dummy variables
 	printf("Enter number of variables in the system:\n");
 	scanf("%d", &n);
 	printf("Enter number of equations in the system\n");
 	scanf("%d", &m);
 	assert(n > m); // Number of variables should be more than the constraints
-	int nbasic = n - m;
-	float *A = (float *)malloc(m * n * sizeof(float));
-	float *B = (float *)malloc(m * sizeof(float));
+	int nbasic = n - m; // Number of non-basic variables 
+	// Coefficient matrix
+	double **A = (double **)malloc(m * sizeof(double *));
+	for(i = 0; i < m; i++)
+		A[i] = (double *)malloc(n * sizeof(double));
+	// Output matrix (Y - values)
+	double **B = (double **)malloc(m * sizeof(double *));
+	for(i = 0; i < m; i++)
+		B[i] = (double *)malloc(sizeof(double));
 	printf("Enter coefficients of constraints equations in matrix form:");
 	getMatrix(A, m, n); // Get A
 	printf("Enter the right side values of the equations:");
 	getMatrix(B, m, 1); // Get B
-	printf("Rank of matrix A is:\n");
-	printf("%d", calculateRank(A, m, n));
+	float *C = augmentMatrices(A, B, m, n); // Make (A | b)
+	if (calculateRank(A, m, n) == calculateRank(C, m, n))
+		printf("\nConsistent system!");
+	else {
+		printf("\nNo solution!")
+		exit(0);
+	}
+	// Number of possible cases or basic solutions
+	long long basic_sols = C(n, m);
+	// Solution matrix containing basic solutions to the problem
+	double **solution = (double **)malloc(m * sizeof(double *));
+	for(i = 0; i < m; i++)
+		solution[i] = (double *)malloc(basic_sols * sizeof(double));
+	// Free dynamically allocated memory 
 	free(A);
 	free(B);
+	free(C);
 }
 
-void swap(float *A, int r1, int r2, int cols)
+double** augmentMatrices(double **A, double **b, int m, int n) {
+	int i, j;
+	double **result = (double **)malloc(m * sizeof(double *));
+	for(i = 0; i < m; i++)
+		result[i] = (double *)malloc((n + 1) * sizeof(double));
+	for(i = 0; i < m; i++) {
+		for(j = 0; j < n; j++)
+			result[i][j] = A[i][j];
+	}
+	for(i = 0; i < m; i++)
+		result[i][n] = b[i][0];
+	return result;
+}
+
+void swap(double **A, int r1, int r2, int cols)
 {
 	int i;
     for(i = 0; i < cols; i++) {
-        float temp = *(A + r1 * cols + i);
-        *(A + r1 * cols + i) = *(A + r2 * cols + i);
-		*(A + r2 * cols + i) = temp;
+        double temp = A[r1][i];
+        A[r1][i] = A[r2][i];
+        A[r2][i] = temp;
     }
 }
 
-int calculateRank(float *A, int rows, int cols){
+int calculateRank(double **A, int rows, int cols){
 	int i, j, k;
 	for(i = 0; i < cols; i++) {
-		printMatrix(A, rows, cols);
-		if(*(A + i * cols + i))
+		// printMatrix(A, rows, cols);
+		if(fabs(A[i][i]) != 0.00)
 			for(j = 0; j < rows; j++)
 				if(j != i) {
 					/* Make all the elements above and below the current principal
                  diagonal element zero */
-                float ratio = (*(A + j * cols + i)/ *(A + i * cols + i));
+                double ratio = (double) A[j][i] / A[i][i];
                 for(k = 0; k < cols; k++)
-                    *(A + j * cols + k) -= ratio * (*(A + i * cols + k));
+                    A[j][k] -= ratio * A[i][k];
 				}
 				else
 					printf("\n");
 		else
 			for(j = i + 1; j < rows; j++)
-				if(*(A + j * cols + i)) {
+				if(A[j][i] != 0) {
 					swap(A, i, j, cols);
 					break;
 				}
@@ -69,25 +109,37 @@ int calculateRank(float *A, int rows, int cols){
 			if(j == rows){
 				--cols;
                 for(j = 0; j < rows; j++)
-                    *(A + j * cols + i) = *(A + (j + 1) * cols);
+                    A[j][i] = A[j][cols];
 			}
 			--i;
 	}
 	return cols;
 }
 
-int printMatrix(float *A, int rows, int cols) {	
+void printMatrix(double **A, int rows, int cols) {	
 	int i, j;
 	for(i = 0; i < rows; i++) {
 		printf("\n");
 		for(j = 0; j < cols; j++)
-			printf("%f ", *(A + i * cols + j));
+			printf("%lf ", A[i][j]);
 	}
 }
 
-int getMatrix(float *A, int rows, int cols) {
+void getMatrix(double **A, int rows, int cols) {
 	int i, j;
 	for(i = 0; i < rows; i++)
 		for(j = 0; j < cols; j++)
-			scanf("%f", (A + i * cols + j));
+			scanf("%lf", &A[i][j]);
 }
+
+long long C(int n, int r) {
+    if(r > n / 2) r = n - r; // because C(n, r) == C(n, n - r)
+    long long ans = 1;
+    int i;
+    for(i = 1; i <= r; i++) {
+        ans *= n - r + i;
+        ans /= i;
+    }
+    return ans;
+}
+
