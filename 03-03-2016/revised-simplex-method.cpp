f7d3dd19d446507c77fe2b2e76c8d6fd124c3476
Abhinav Jain <abhinavjain241@gmail.com>
2016-05-06 23:50:27 +0200

    Modified Revised Simplex Method

---
+++ 03-03-2016/revised-simplex-method.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main() {
+	//*****************Inputs********************
+	cout << "Enter the number of equations and number of variables" << endl;
+	cin >> n >> m;
+	int kk = m;
+	double AA[n+1][m+1], BB[n+1][m+1], d, b[n], M=10000;
+	cout<<"Enter the coefficients of the variables in the constraints"<<endl;
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			cin>>AA[i][j];
+}
--- 03-03-2016/revised.cpp
+++ 03-03-2016/revised.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 #define SIZE 15
 
-double a[SIZE][SIZE], b[SIZE][SIZE], c[SIZE][SIZE], temp[SIZE][SIZE], t[SIZE][SIZE], d[SIZE][SIZE], ans[SIZE][SIZE], B[SIZE], C[SIZE], CB[SIZE], Y[SIZE], T[SIZE][SIZE], XB[SIZE], aplha[SIZE], k[SIZE][SIZE];
+double a[SIZE][SIZE], b[SIZE][SIZE], c[SIZE][SIZE], temp[SIZE][SIZE], t[SIZE][SIZE], d[SIZE][SIZE], ans[SIZE][SIZE], B[SIZE], C[SIZE], CB[SIZE], Y[SIZE], T[SIZE][SIZE], XB[SIZE], alpha[SIZE], k[SIZE][SIZE];
 
 void getMat(double a[][SIZE], int row , int col) {
 	int i, j;
@@ -52,13 +52,13 @@ void multiMat(double a[][SIZE], double b[][SIZE], int row, int col){
 		for(j = 0; j < col; j++){
 			sum  = 0;
 			for(k = 0; k < col; k++){
-				
 				sum += a[i][k] * b[k][j];
 			}
 			ans[i][j] = sum;
 		}
-
 }
+
+
 void check(double a[][SIZE], int row, int col){
 	int i,j;
 	double p;
@@ -85,9 +85,9 @@ void multi1(double a[][SIZE], double b[][SIZE], int row, int col, int j){
 	}
 }
 
-void reverse(double a[][SIZE], int row, int col){
+void reverse(double a[][SIZE], int row, int col) {
 	int i, j, m;
-	for(i = 0; i < row; i++){
+	for(i = 0; i < row; i++) {
 		temp[i][0] = a[i][0];
 	}
 	for(i = 0; i < row ; i++)
@@ -99,13 +99,12 @@ void reverse(double a[][SIZE], int row, int col){
 	for(i = 0; i < row ; i++)
 			for(j =0; j < col; j++)
 				c[i][j] = temp[i][j];
-	for(m = 1; m < col; m++){
+	for(m = 1; m < col; m++) {
 		for(i = 0; i < row ; i++)
 			for(j =0; j < col; j++)
 				t[i][j] = b[i][j];
-		
 
-		for(i = 0; i < row; i++){
+		for(i = 0; i < row; i++) {
 			t[i][m] = a[i][m];
 		}
 		multi1(c, t, row, col, m);
@@ -123,22 +122,21 @@ void reverse(double a[][SIZE], int row, int col){
 	printMatrix(ans, row, col);
 }
 
-void yValue(double b[SIZE], double a[][SIZE], int row, int col){
+void yValue(double b[SIZE], double a[][SIZE], int row, int col) {
 	int i, k;
 	double sum;
-	for(i = 0; i < row; i++){
+	for(i = 0; i < row; i++) {
 		sum  = 0;
-		for(k = 0; k < col; k++){
-				
+		for(k = 0; k < col; k++) {
 			sum += b[k] * a[k][i];
 		}
 		Y[i] = sum;
 	}
 }
-double mul(double y[SIZE], double a[][SIZE],int row,int col){
+double mul(double y[SIZE], double a[][SIZE],int row,int col) {
 	int i, j;
 	double sum;
-	for(i = 0; i < row; i++){
+	for(i = 0; i < row; i++) {
 		sum  += y[i] * a[i][col];
 	}
 	return sum;
@@ -149,12 +147,12 @@ int entering(double y[SIZE], double a[][SIZE], double c[SIZE], int row, int col)
 	double temp;
 	double min = 0;
 	bool t = false;
-	for(i = 0; i < col; i++){
+	for(i = 0; i < col; i++) {
 		temp = mul(y, a, row, i);
 		temp = temp - c[i];
-		if(temp < 0){
+		if(temp < 0) {
 			t = true;
-			if(temp < min){
+			if(temp < min) {
 				min = temp;
 				index = i;
 			}
@@ -169,29 +167,25 @@ int entering(double y[SIZE], double a[][SIZE], double c[SIZE], int row, int col)
 void setXB(double a[][SIZE], double b[SIZE],int row,int col){
 	int i, k;
 	double sum;
-	for(i = 0; i < row; i++){
-		sum  = 0;
-		for(k = 0; k < col; k++){
-				
+	for(i = 0; i < row; i++) {
+		sum = 0;
+		for(k = 0; k < col; k++) {
 			sum += a[i][k] * b[k];
 		}
 		XB[i] = sum;
 	}
-
 }
 
 void setAlpha(double a[][SIZE], double b[][SIZE],int row,int col){
 	int i, k;
 	double sum;
-	for(i = 0; i < row; i++){
-		sum  = 0;
-		for(k = 0; k < row; k++){
-				
+	for(i = 0; i < row; i++) {
+		sum = 0;
+		for(k = 0; k < row; k++) {
 			sum += a[i][k] * b[k][col];
 		}
-		aplha[i] = sum;
+		alpha[i] = sum;
 	}
-
 }
 
 int main(){
@@ -207,7 +201,6 @@ int main(){
 	getVector(B, row);
 	std::cout << "Enter the coefficients and constant for the objective function" << std::endl;
 	getVector(C, col);
-
 	for(i = 0; i < row ; i++)
 		for(j =0; j < row; j++)
 			T[i][j] = b[i][j];
@@ -218,7 +211,6 @@ int main(){
 	for(i = 0; i < row; i++)
 			for(j = 0 ; j < row; j++)
 				k[i][j] = T[i][j];
-
 	setXB(T, B, row, row);
 	while(entering(Y, a, C, row, col) >= 0){
 		int enter, leave;
@@ -227,18 +219,16 @@ int main(){
 		enter = entering(Y, a, C, row, col);
 		
 		setAlpha(T, a, row, enter);
-		for(i = 0; i < row; i++){
-			if(aplha[i] != 0){
-				ratio = XB[i] / aplha[i];
-				if(ratio < min){
+		for(i = 0; i < row; i++) {
+			if(alpha[i] != 0) {
+				ratio = XB[i] / alpha[i];
+				if(ratio < min) {
 					min = ratio;
 					leave = i;
 				}
-
 			}
 		}
-		
-		for(i = 0; i < row; i++){
+		for(i = 0; i < row; i++) {
 			k[i][leave] = a[i][enter];
 		}
 		
@@ -247,22 +237,21 @@ int main(){
 			for(j = 0 ; j < row; j++)
 				T[i][j] = ans[i][j];
 
-		for(i = 0; i < row; i++){
-			if(i == leave){
+		for(i = 0; i < row; i++) {
+			if(i == leave) {
 				CB[i] = C[enter];
 			}
 		}
 		C[enter] = 0;
-		for(i = 0; i < row; i++){
+		for(i = 0; i < row; i++) {
 			a[i][enter] = 0;
 			if(i == enter)
 				a[i][enter] = 1;
 		}	
 		setXB(T, B, row, row);
-
 	}
 	double solution = 0;
-	for(i = 0; i < row; i++){
+	for(i = 0; i < row; i++) {
 		solution += CB[i] * XB[i];
 	}
 	std::cout << "The solution of LPP : " << solution << std::endl;
